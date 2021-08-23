774ef2c8b1c69f330044d40324713c036a3f1edc
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2021-08-23 12:48:29 -0400

    Game over

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,9 +37,12 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_acive:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):
@@ -157,19 +160,22 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
-        # Decrement ships_left
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Decrement ships_left
+            self.stats.ships_left -= 1
 
-        # Get rid of any remaining aliens and bullets.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # create a new fleet and center the ship.
-        self._create_fleet()
-        self.ship.center_ship()
+            # create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pause.
-        sleep(0.5)
+            # Pause.
+            sleep(0.5)
+        else:
+            self.stats.game_acive = False
 
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached and edge."""
